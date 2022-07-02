48e1ba93b2bf742a65fc630256b64cad0a4a3e01
Jackson Clarke <104281508+XA1phaX@users.noreply.github.com>
2022-07-01 19:48:22 -0600

    Finished Python Calculator
    Co-authored-by: Nlloyd21 <Nlloyd21@users.noreply.github.com>

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # Team: Backcorner
 # Members: Jackson Clarke, Noah Lloyd, Franck Sokora, and Spencer Wilson
 # June 2022
+# Total time: 10 hours
 
 ### Imports ###
 import math
@@ -10,7 +11,8 @@ import math
 
 ### Variables ###
 isRunning = True
-runningTotal = 0
+listOfTotals = []
+
 
 ### Functions ###
 
@@ -38,34 +40,10 @@ def divide(x, y):
     return total
 
 
-# Area of a circle
-def area_circle(radius):
-    total = math.pi * radius**2
-    return total
-
-
-# Area of a square
-def area_square(side):
-    total = side**2
-    return total
-
-
-# Area of a rectangle
-def area_rectangle(l, w, h):
-    total = l * w * h
-    return total
-
-
-# Exponent
-def exponent(x, y):
-    total = x**y
-    return total
-
-
 # Clear
 def clear():
-    global runningTotal
-    runningTotal = 0
+    global listOfTotals
+    listOfTotals = []
 
 
 # Quit
@@ -77,19 +55,102 @@ def quit():
 # Error
 def checkError(x, y):
     try:
-        x = int(x)
-        y = int(y)
+        x = float(x)
+        y = float(y)
     except ValueError:
         print("Error: Please enter a number.")
         return True
 
 
 ### Main ###
-def main():
-    x = float(input("Please enter first number "))
-    y = float(input("Please enter the second number "))
-    operator = input(
-        "Please select an operator\n1 = +\n2 = -\n3 = *\n4 = /\n\
-    5 = Area of a circle\n6 = Area of a square\n7 = Area of a rectangle\n8 = Exponent\n\
-    9 = Clear\n10 = Quit\n"
-    )
+print("\nWelcome to the python calculator!\n")
+while isRunning == True:
+    if len(listOfTotals) < 1:
+        x = float(input("Please enter first number: "))
+        operator = int(
+            input(
+                "Please select an operator\n1 = +\n2 = -\n3 = *\n4 = /\n\
+9 = Clear\n0 = Quit\n\nOperator Number: "
+            )
+        )
+        y = float(input("Please enter the second number: "))
+
+        if operator == 1:
+            answer = add(x, y)
+            print(answer)
+            listOfTotals.append(answer)
+
+        elif operator == 2:
+            answer = subtract(x, y)
+            print(answer)
+            listOfTotals.append(answer)
+
+        elif operator == 3:
+            answer = multiply(x, y)
+            print(answer)
+            listOfTotals.append(answer)
+
+        elif operator == 4:
+            answer = divide(x, y)
+            print(answer)
+            listOfTotals.append(answer)
+
+        elif operator == 9:
+            clear()
+            print("Cleared")
+
+        elif operator == 0:
+            quit()
+
+        else:
+            print("Error: Please enter a number.")
+    else:
+        counter = len(listOfTotals) - 1
+        x = listOfTotals[counter]
+        operator = int(
+            input(
+                "\nPlease select an operator\n1 = +\n2 = -\n3 = *\n4 = /\n\
+9 = Clear\n0 = Quit\n\nOperator Number: "
+            )
+        )
+
+        if operator == 9:
+            clear()
+            print("Cleared")
+        elif operator == 0:
+            quit()
+        else:
+            y = float(input("Please enter the second number: "))
+
+            if operator == 1:
+                answer = add(x, y)
+                print(answer)
+                listOfTotals.append(answer)
+
+            elif operator == 2:
+                answer = subtract(x, y)
+                print(answer)
+                listOfTotals.append(answer)
+
+            elif operator == 3:
+                answer = multiply(x, y)
+                print(answer)
+                listOfTotals.append(answer)
+
+            elif operator == 4:
+                answer = divide(x, y)
+                print(answer)
+                listOfTotals.append(answer)
+
+            elif operator == 9:
+                clear()
+                print("Cleared")
+
+            elif operator == 0:
+                quit()
+
+            else:
+                print("Error: Please try again.")
+# End of while loop
+
+print("Thanks for using our python calculator!")
